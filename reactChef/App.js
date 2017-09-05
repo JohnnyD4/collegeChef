4213172221ffce4f881fd6d874e336396f7e1bed
John Davis <johnsdavis95@gmail.com>
2017-09-05 00:07:59 -0400

    added edamam api and tested

--- reactChef/App.js
+++ reactChef/App.js
@@ -5,7 +5,7 @@ export default class App extends React.Component {
   componentDidMount() {
     console.log("obj");
     return fetch("https://api.edamam.com/search?q=chicken&app_id=63f8bc89&app_key=6102576183eb6fec6ef9ac523922c97c&from=1&to=1")
-    .then((response) => console.log("body", response["_bodyText"]["q"]))
+    .then((response) => console.log("body", response["_bodyText"]))
   }
 
   render() {
