c4f3c49d08d4148d9f0d50b6b2ebe29c266deb72
John Davis <johnsdavis95@gmail.com>
2017-09-07 21:48:30 -0400

    trying to get name of recipe to appear on page

--- reactChef/App.js
+++ reactChef/App.js
@@ -2,20 +2,34 @@ import React from 'react';
 import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state={
+        label:'',
+        password:''
+        }
+
+     }
+
   componentDidMount() {
     console.log("obj");
     return fetch("https://api.edamam.com/search?q=chicken,rice&app_id=63f8bc89&app_key=6102576183eb6fec6ef9ac523922c97c&from=0&to=1")
     
     .then(function(response){
         var res = JSON.parse(response._bodyText);
-        console.log("body", res.hits[0].recipe.image);
+        console.log("body", res.hits[0].recipe.label);
+        var label = res.hits[0].recipe.label;
+        this.label = label;
+        
+        console.log("label", this.label);
+        
     })
   }
 
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Text>{this.label}</Text>
         <Text>Changes you make will aufdgdfgtomatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
           <Image source={{
